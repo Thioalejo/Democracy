21b90f3f62dc4855f3ee24f57046e4f77ba3c06f
alejo <alejo.saraza@hotmail.es>
2016-10-20 14:22:38 -0500

    Creamos States index y creamos acciones

---
+++ Democracy/Controllers/StatesController.cs
@@ -0,0 +1,57 @@
+﻿using Democracy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Democracy.Controllers
+{
+    public class StatesController : Controller
+    {
+        //clase para conectarme a base de datos
+        private DemocratyContext db = new DemocratyContext();
+        // GET: States
+
+       ///para crear lista de estados
+        public ActionResult Index()
+        {
+            return View(db.States.ToList());
+        }
+
+        //para crear la accion, para crear lista o llenar tabla
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //se sobre escribe el metodo crear para que al precionar el boton crear si cree
+        [HttpPost]
+        public ActionResult Create(State state)
+        {
+            //si el modelo no es valido devuelvase
+            if(!ModelState.IsValid)
+            {
+                return View(state);
+            }
+
+            //mandamos el objeto a la base de datos, es decir asi grabamos en base de datos
+            db.States.Add(state);
+            db.SaveChanges();
+
+            //para retornar a la misma vista dejamos return View, pero para 
+            //retornar otra vista ponemos RedirectToAction y el nombre de la vista
+            return RedirectToAction("Index");
+        }
+
+        //cerrar conexion bd
+        protected override void Dispose(bool disposing)
+        {
+
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Democracy/Models/DemocratyContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Democracy.Models
+{
+
+    //para conectarse a la base de datos
+    public class DemocratyContext: DbContext
+    {
+
+       public DemocratyContext() : base("DefaultConnection") //nombre de la cadena de conexion
+        {
+
+        }
+        //se van agregando las propiedades (Dbset) a medida que se quiere enviar datos a determindaas tablas de base de datos
+        public DbSet<State> States { get; set; }
+    }
+}
